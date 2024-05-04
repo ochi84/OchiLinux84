b8235acea0d190c17a37c1b8bb6e00ec6f27e959
ochi84 <ambidexxxter@gmail.com>
2024-05-04 14:07:31 +0200

    updated

--- rc.lua
+++ rc.lua
@@ -530,8 +530,8 @@ awful.rules.rules = {
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+     { rule = { class = "firefox" },
+       properties = { screen = 1, tag = "2" } },
 }
 -- }}}
 
