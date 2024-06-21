8516ff05add448eedec9c692658d290b685218d0
ochi84 <ambidexxxter@gmail.com>
2024-06-21 08:33:50 +0200

    Added key-binding for qutebrowser

--- rc.lua
+++ rc.lua
@@ -414,7 +414,10 @@ awful.key({}, "XF86AudioMute", function () awful.util.spawn("amixer -D pulse sse
                  {description = "launch browser", group = "launcher"}),
        -- thunar
        awful.key({ modkey }, "t", function() awful.util.spawn ("thunar") end,
-                 {description = "launch thunar", group = "launcher"})
+                 {description = "launch thunar", group = "launcher"}),
+       -- qutebrowser
+       awful.key({ modkey }, "q", function() awful.util.spawn ("qutebrowser") end,
+                 {description = "launch qutebrowser", group = "launcher"})
 )
 
 clientkeys = gears.table.join(
